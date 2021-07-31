2bf87453f7a5ed7801b60011cb338d226b0e80e4
takayama shunya <katonbo1@icloud.com>
2021-07-31 11:31:40 +0900

    memoモデル、テーブル作成

---
+++ database/migrations/2021_07_31_013228_create_memos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title','50')->nullable();
+            $table->string('content','2000');
+            $table->foreignId('user_id')->ondelete('cascade')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
