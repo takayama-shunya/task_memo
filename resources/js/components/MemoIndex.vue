218e8f62d72885e5efac08082c46263c8e948c28
takayama shunya <katonbo1@icloud.com>
2021-11-03 18:03:01 +0900

    vue index 表示修正

--- resources/js/components/MemoIndex.vue
+++ resources/js/components/MemoIndex.vue
@@ -1,17 +1,36 @@
 <template>
 <div>
 	<h1>memo一覧</h1>
-	<ul>
-		<li 
-      v-for="memo in tasks" 
-      v-text="memo.title" 
-    ></li>
-	</ul>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th class="">タイトル</th>
+				<th class="">作成日</th>
+				<th class="">内容</th>
+				<th class=""></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="(memo, index) in tasks" :key=index >
+				<td>{{ memo.title }}</td>
+				<td>{{ memo.created_at | moment }}</td>
+				<td>{{ memo.content }}</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
+
+
 </template>
 
 <script>
+  import moment from "moment"
   export default {
+		filters: {
+        moment: function (date) {
+            return moment(date).format('YYYY/MM/DD HH:mm');
+        }
+    },
     data: function () {
           return {
               tasks: [],
@@ -25,6 +44,6 @@
 				.catch(error => {
 					console.log(error)
 				});
-		} 
+		},
   }
 </script>
